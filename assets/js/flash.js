420cabe6d197dbd3b9da6afcef18994953c1e1c3
Luis Campos <falcon758@users.noreply.github.com>
2022-02-23 01:03:00 +0000

    Flash messages implementation

--- assets/js/flash.js
+++ assets/js/flash.js
@@ -1,6 +1,8 @@
 (function($) {
     const flashContainer = $('#flash-messages');
 
+    const messageValidator = RegExp('^[a-zA-Z0-9\\s]+$');
+
 	let flashController = function() {
 		return {
 			clearAll: function() {
@@ -22,27 +24,25 @@
                     case 'error':
                         divClass += ' alert-danger';
                         break;
-            
                     case 'warning':
                         divClass += ' alert-warning';
                         break;
-            
                     case 'notice':
                         divClass += ' alert-info';
                         break;
-            
                     case 'success':
                         divClass += ' alert-success';
                         break;
-            
                     default:
                         divClass += ' alert-danger';
                         break;
                 }
-            
-                let flash = $('<div></div>').text(message).addClass(divClass);
-                flashContainer.append(flash);
-                this.setClearTimer(flash);
+
+                if (message.match(messageValidator) !== null) {
+                    let flash = $('<div></div>').text(message).addClass(divClass);
+                    flashContainer.append(flash);
+                    this.setClearTimer(flash);
+                }
             }
 		};
 
