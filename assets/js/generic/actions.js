c7a00eabdecfbc897b8becde061442977453bad8
Luis Campos <falcon758@users.noreply.github.com>
2022-02-26 01:24:59 +0000

    Response message implementation

--- assets/js/generic/actions.js
+++ assets/js/generic/actions.js
@@ -4,12 +4,16 @@
         const row = $(this).parents('tr');
         const recordId = row.attr('record-id');
 
-        if(confirm('Are you sure you want do delete id: ' + recordId)) {
+        if (confirm('Are you sure you want do delete id: ' + recordId)) {
             const route = $(this).attr('delete-route');
             const csrf = $(this).parents('tbody').attr('csrf-token');
 
             $.post(route, 'csrf=' + csrf, function(response) {
-                if(response.status === 1) {
+                let status = response.success === true;
+                let responseStatus = status ? 'success' : 'error';
+                $.fn.flashMessage().flashAdd(response.message, responseStatus);
+
+                if (status) {
                     row.remove();
                 }
             });
